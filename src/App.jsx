a18cb2a7eb2074391fdb8dcdcd0295d4011a4dd5
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2025-03-23 19:25:25 +0530

    Added Models and Basic Files Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FaceDetection from "./components/FaceDetection";
+
+function App() {
+  return (
+    <div className="container flex flex-col gap-3 mt-3" >
+      <h1 className="text-2xl font-bold text-center bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text">
+        Face Detection Testing
+      </h1>
+      <FaceDetection />
+    </div>
+  );
+}
+
+export default App;
